5a75cdc9ba4ff197ba4722267c1895cab7e8b731
Daniel Anderson <daniel.j.anderson.dev@gmail.com>
2023-01-26 00:50:39 -0500

    added keyboard input

--- src/main.cpp
+++ src/main.cpp
@@ -2,8 +2,13 @@
 #include "../include/Point.h"
 #include "../include/Shape3D.h"
 #include "../include/Quaternion.h"
-#include <iostream>
-#include <cmath>
+#include <string>
+const int WINDOW_WIDTH  = 1920;
+const int WINDOW_HEIGHT = 1080;
+const Point ORIGIN = Point(WINDOW_WIDTH/2, WINDOW_HEIGHT/2, 0);
+const Point xAxis  = Point(1, 0, 0);
+const Point yAxis  = Point(0, 1, 0);
+const Point zAxis  = Point(0, 0, 1);
 
 void drawLine(SDL_Renderer* renderer, Point a, Point b)
 {
@@ -18,21 +23,49 @@ void clearScreen(SDL_Renderer* renderer)
 	SDL_RenderClear(renderer);
 }
 
-double lengthOfLine(Point a, Point b)
+void handleInput(SDL_Event event, Point &point)
 {
-	double dx  = b.getX() - a.getX();
-	double dy  = b.getY() - a.getY();
-	double dz  = b.getZ() - a.getZ();
-	double dx2 = dx * dx;
-	double dy2 = dy * dy;
-	double dz2 = dz * dz;
-	return sqrt(dx2 + dy2 + dz2);
+	if (SDL_PollEvent(&event))
+	{
+		if (event.type == SDL_KEYDOWN)
+		{
+			SDL_Keycode key = event.key.keysym.sym;
+			if (key == SDLK_a)
+			{
+				point.rotate(ORIGIN, xAxis, .01);
+			}
+			if (key == SDLK_d)
+			{
+				point.rotate(ORIGIN, xAxis, -.01);
+			}
+			if (key == SDLK_w)
+			{
+				point.rotate(ORIGIN, yAxis, .01);
+			}
+			if (key == SDLK_s)
+			{
+				point.rotate(ORIGIN, yAxis, -.01);
+			}
+			if (key == SDLK_q)
+			{
+				point.rotate(ORIGIN, zAxis, .01);
+			}
+			if (key == SDLK_e)
+			{
+				point.rotate(ORIGIN, zAxis, -.01);
+			}
+
+			if (key == SDLK_ESCAPE)
+			{
+				SDL_Quit();
+				exit(0);
+			}
+		}
+	}
 }
 
 int main(int argc, char *argv[])
 {
-	const int WINDOW_WIDTH  = 640;
-	const int WINDOW_HEIGHT = 480;
 	// Initialize SDL
 	SDL_Init(SDL_INIT_VIDEO);
 
@@ -40,33 +73,19 @@ int main(int argc, char *argv[])
 	SDL_Window*   window   = SDL_CreateWindow("Platonic Solids", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, WINDOW_WIDTH, WINDOW_HEIGHT, SDL_WINDOW_SHOWN);
 	SDL_Renderer* renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
 
-	// Create points to draw a line between
-	// and angle and axis to rotate about
-	Point  point  = Point(500, WINDOW_HEIGHT/2, 0);
-	Point  origin = Point(WINDOW_WIDTH/2, WINDOW_HEIGHT/2, 0);
+	// Create point to draw a line between and angle and axis to rotate about
+	Point  point  = Point(250, WINDOW_HEIGHT/2, 0);
 	Point  axis   = Point(0, 0, 1);
-	double angle  = .01;
-
-	int counter = 0;
-
+	double angle  = 1;
+	
 	SDL_Event event;
 	while (true)
 	{
-		// TODO: Handle quit event
-		if (SDL_PollEvent(&event) && event.type == SDL_QUIT)
-		{
-			break;
-		}
-
 		clearScreen(renderer);
 
-		drawLine(renderer, point, origin);
+		drawLine(renderer, point, ORIGIN);
 
-		point.rotate(origin, axis, angle);
-
-		// Wait about 1/60 of a second
-		SDL_Delay(16.67);
-		counter++;
+		handleInput(event, point);
 	}
 
 	// Clean up
