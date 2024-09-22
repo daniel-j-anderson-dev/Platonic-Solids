c5b986335511aaee5ba4652db70c6d46e86e8245
Daniel Anderson <daniel.j.anderson.dev@gmail.com>
2024-09-22 09:54:25 -0600

    use cmake instead of make

--- include/Renderer.h
+++ include/Renderer.h
@@ -1,10 +1,11 @@
 #ifndef Renderer_H
 #define Renderer_H
+#include <array>
+#include <span>
 #pragma once
-#include "../include/SDL2/SDL.h"
+#include <SDL3/SDL.h>
 #include "../include/Shape3D.h"
 #include "../include/Transformer.h"
-#include <cmath>
 
 struct rgbaColor
 {
@@ -24,19 +25,19 @@ public:
     Point xAxis();
     Point yAxis();
     Point zAxis();
-    std::vector<Shape3D> *getAxes();
-    std::vector<Shape3D> *getShapes();
+    std::span<Shape3D, 3> getAxes();
+    std::span<Shape3D, 5> getShapes();
     std::vector<Point> allAxes();
     std::pair<Point, double> getWorldOrientation();
 
     // mutators
     void axesDefault();
-    void setShapes(std::vector<Shape3D> *shapes);
+    void setShapes(const std::array<Shape3D, 5> shapes);
 
     // actions
     void drawLine(rgbaColor color, Point startPoint, Point endPoint);
-    void drawShape(Shape3D shape);
-    void drawShapes(std::vector<Shape3D> shapes);
+    void drawShape(const Shape3D& shape);
+    void drawShapes();
     void drawAxes();
 
     void clearScreen();
@@ -45,14 +46,14 @@ public:
 
 private:
 
-    SDL_Window*          window;
-    SDL_Renderer*        renderer2D;
-    SDL_Texture*         background;
-    Point                ORIGIN;
-    std::vector<Shape3D> *shapes;
-    std::vector<Shape3D> axes;
-    Uint64               startTime;
-    Uint64               endTime;
+    SDL_Window* window;
+    SDL_Renderer* renderer2D;
+    SDL_Texture* background;
+    Point ORIGIN;
+    std::array<Shape3D, 5>     shapes;
+    std::array<Shape3D, 3> axes;
+    Uint64 startTime;
+    Uint64 endTime;
 };
 
 #endif
--- include/Transformer.h
+++ include/Transformer.h
@@ -1,5 +1,7 @@
 #ifndef TRANSFORMER_H
 #define TRANSFORMER_H
+#include <array>
+#include <span>
 #pragma once
 #include "../include/Point.h"
 #include "../include/Shape3D.h"
@@ -7,17 +9,17 @@
 #include "../include/Renderer.h"
 #include <cmath>
 
-std::vector<Shape3D> platonicSolids();
+std::array<Shape3D, 5> platonicSolids();
 void rotatePoint(Point &point, Point axis, double angle);
 void rotatePointAboutAnother(Point &point, Point centerOfRotation, Point axis, double angle);
 void rotateShapeLocal(Shape3D &shape, Point axis, double angle);
 void rotateShapeAboutPoint(Shape3D &shape, Point centerOfRotation, Point axis, double angle);
-void rotateShapesLocal(std::vector<Shape3D> &shapes, Point axis, double angle);
-void rotateShapesAboutPoint(std::vector<Shape3D> &shapes, Point centerOfRotation, Point axis, double angle);
+void rotateShapesLocal(std::span<Shape3D> shapes, Point axis, double angle);
+void rotateShapesAboutPoint(std::span<Shape3D> shapes, Point centerOfRotation, Point axis, double angle);
 
 void translatePoint(Point &point, Point axis, double distance);
 void tanslateShape(Shape3D &shape, Point axis, double distance);
-void translateShapes(std::vector<Shape3D> &shapes, Point axis, double distance);
+void translateShapes(std::span<Shape3D> shapes, Point axis, double distance);
 
 double dotProduct(Point u, Point v);
 Point crossProduct(Point u, Point v);
